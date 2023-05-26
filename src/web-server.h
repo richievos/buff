3b4895fc411b5121ff3caef5b097df2ea0f61b7c
Richie Vos <vosrichie@gmail.com>
2023-05-25 21:48:07 -0700

    Add a json endpoint to fetch readings

--- src/web-server.h
+++ src/web-server.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <Arduino.h>
+#include <ArduinoJson.h>
 #include <WebServer.h>  // Built into ESP32
 
 #include <string>
@@ -25,7 +26,7 @@ class BuffWebServer {
     std::unique_ptr<alk_measure::TriggerRequest> _pendingTrigger;
 
    public:
-    BuffWebServer(std::shared_ptr<buff_time::TimeWrapper> timeClient, int port=80) : _server(port), _timeClient(timeClient) {}
+    BuffWebServer(std::shared_ptr<buff_time::TimeWrapper> timeClient, int port = 80) : _server(port), _timeClient(timeClient) {}
 
     void handleRoot() {
         std::string bodyText;
@@ -86,11 +87,43 @@ class BuffWebServer {
         _server.send(404, "text/plain", message);
     }
 
+    void handleGetReadings() {
+        DynamicJsonDocument responseDoc(1024);
+
+        responseDoc["asOfMS"] = millis();
+        responseDoc["asOfAdjustedSec"] = _timeClient->getAdjustedTimeSeconds();
+
+        auto readings = _readingStore->getReadingsSortedByAsOf();
+        const size_t limit = 10;
+        const auto size = std::min(limit, readings.size());
+        responseDoc["size"] = size;
+
+        readings  =
+            std::vector<std::reference_wrapper<alk_measure::PersistedAlkReading>>(readings.begin(),
+                                                                                  readings.begin() + size);
+        auto readingsDoc = responseDoc.createNestedArray("readings");
+
+        for (const auto reading : readings) {
+            // TODO: replace with convertToJson(const tm& src, JsonVariant dst)
+            auto jsonReading = readingsDoc.createNestedObject();
+            jsonReading["asOfAdjustedSec"] = reading.get().asOfAdjustedSec;
+            jsonReading["alkReadingDKH"] = reading.get().alkReadingDKH;
+            jsonReading["title"] = reading.get().title.c_str();
+        }
+
+        String serializedDoc;
+        serializeJson(responseDoc, serializedDoc);
+        Serial.print("sending: ");
+        Serial.println(serializedDoc);
+        _server.send(200, "application/json", serializedDoc);
+    }
+
     void setupWebServer(std::shared_ptr<reading_store::ReadingStore> rs) {
         _readingStore = rs;
 
         _server.on("/", [&]() { handleRoot(); });
         _server.on("/execute/measure_alk", [&]() { handleTrigger(); });
+        _server.on("/readings.json", [&]() { handleGetReadings(); });
         _server.onNotFound([&]() { handleNotFound(); });
         _server.begin();
         Serial.println("HTTP server started");
